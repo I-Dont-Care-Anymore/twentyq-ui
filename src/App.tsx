15832182766cc44fe6eb0060400635b8373eef96
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-02 23:37:11 -0500

    Yeet

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
 
 class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to
-                    reload.
-                </p>
-            </div>
+            <Router>
+                <div className="App">
+                    <Route path="/" exact={true} component={LandingPage} />
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/LandingPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { H1 } from '@blueprintjs/core';
+
+class LandingPage extends React.Component {
+    public render() {
+        return <H1>Yeet</H1>;
+    }
+}
+
+export default LandingPage;
