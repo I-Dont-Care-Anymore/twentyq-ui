b9d2c08a6e0feb6b1d19b89b133826bffcb9ac98
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-02 23:39:01 -0500

    Add react-router stuff

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 
 import { H1 } from '@blueprintjs/core';
+import { RouteComponentProps } from 'react-router-dom';
 
-class LandingPage extends React.Component {
+class LandingPage extends React.Component<RouteComponentProps<any>, {}> {
+    constructor(props: RouteComponentProps<any>) {
+        super(props);
+    }
+
     public render() {
         return <H1>Yeet</H1>;
     }
